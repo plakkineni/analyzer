dcc801a269f71d69c5fecb9911627c4a9162f891
root <root@pythonvm.us-west2-a.c.alpine-alpha-233301.internal>
2019-04-09 22:44:56 +0000

    Use of click module

--- snapshot/aws.py
+++ snapshot/aws.py
@@ -1,9 +1,16 @@
 import boto3
+import click
 
+session=boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
 
-if '__name__' == '__main__':
- isession=boto3.Session(profile_name='shotty')
- ec2 = session.resource('ec2')
-
+@click.command()
+def listinstances():
+ "List EC2 Instances"
  for ec in ec2.instances.all():
-  print(ec) 
+  print(','.join((ec.id,ec.instance_type,ec.state['Name'],ec.public_dns_name)))  
+
+ return
+
+if __name__ == '__main__':
+  listinstances()
